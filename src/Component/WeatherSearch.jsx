4d2d8faf73eeb3a24837ff361e87227068e1dca0
Shubham Gupta <guptashubham5991@gmail.com>
2024-02-16 19:40:18 +0530

    weather app started

---
+++ src/Component/WeatherSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WeatherCard from './WeatherCard'
+
+const WeatherSearch = () => {
+  return (
+    <div><WeatherCard/></div>
+  )
+}
+
+export default WeatherSearch
